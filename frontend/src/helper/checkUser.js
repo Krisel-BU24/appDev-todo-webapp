b49f12c0136cb005a736c1d1bb26532ec8f93850
Krisel-BU24 <kriselbiron.banday24@bicol-u.edu.ph>
2025-12-26 14:55:44 +0800

    Fix variable name from 'res' to 'response'

--- frontend/src/helper/checkUser.js
+++ frontend/src/helper/checkUser.js
@@ -1,7 +1,7 @@
 // helper function 
 const checkIfUserExist = async (username) => {
     try {
-        const res = await fetch("http://localhost:3000/api/check-user", {
+        const response = await fetch("http://localhost:3000/api/check-user", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username: username }) // Use the dynamic parameter
